8803dea257ff88002602305f36885765cf23057e
Olivier0123 <45844642+Olivier0123@users.noreply.github.com>
2018-12-13 10:47:08 +0100

    Add files via upload

---
+++ tools.js
@@ -0,0 +1,76 @@
+function truncate(val)
+{
+	return (intval = val - val%1);
+}
+
+function getRandomInt(min, max)
+{
+   return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getRandom(min, max)
+{ return (Math.random() * (max-min+1)) + min; }
+
+
+
+
+function Grille(nbX,nbY,mon_canvas) {
+
+  
+  var dX= 350/nbX;
+  var dY=350/nbY;
+  
+    for(i=1;i<nbY;i++){
+	ctx.moveTo(i*dY,0);
+	ctx.lineTo(i*dY,350);
+	ctx.stroke();
+    }
+	     
+    for(j=1;j<nbX;j++)
+    {
+	ctx.moveTo(0,j*dX);
+	ctx.lineTo(350,j*dX);
+	ctx.stroke();
+    }
+}
+
+function dessinGrille(nbX,nbY,mon_canva){}
+
+function ajoutObjet(objet){}
+
+function dessin(){}
+
+function mvtAdmis(objet,dx,dy){}
+
+function majDessinObjet(objet,dx,dy){}
+
+function stopObjets() {}
+
+
+
+
+function Robot(x,y,vitesse,couleur){}
+
+function dessin(ctx,dx,dy){}
+
+function perception() {}
+
+function reflexion(tabMvt){}
+
+function action(act){}
+
+function reveille(){}
+
+
+
+
+
+
+
+for(i=0;i<nbX;i++){
+	     for(j=0;j<nbY;j++){
+			
+			tab[i][j]=FALSE;
+			}
+  }
+var tab=matrix(nbX,nbY);
