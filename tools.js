d37604047049c0439ab955b686f710e43ead7500
Olivier0123 <45844642+Olivier0123@users.noreply.github.com>
2018-12-13 11:34:44 +0100

    Add files via upload

--- tools.js
+++ tools.js
@@ -13,39 +13,47 @@ function getRandom(min, max)
 
 
 function Grille(nbX,nbY,mon_canvas) {
+    
+    var nbC=nbX*nbY;
+    var nbCur=0;
+    var obj=[];
+    
+    var tab=[];
+    for (i=0;i<nbX;i++) {
+	var ligne=[];
+	for (j=0;j<nbY;j++) {
+	    ligne.push(false);
+	}
+	tab.push(ligne);
+    }
+    return(tab);
+}
+
+
+
+function dessinGrille(nbX,nbY,mon_canvas){
+    var dX= 350/nbX;
+    var dY=350/nbY;
+    
 					  
-  var dX= 350/nbX;
-  var dY=350/nbY;
-					  
-					  
-  for(i=1;i<nbY;i++){
+    for(i=1;i<nbY;i++){
       ctx.moveTo(i*dY,0);
       ctx.lineTo(i*dY,350);
       ctx.stroke();
   }
-						     
+    
     for(i=1;i<nbX;i++){
 	ctx.moveTo(0,i*dX);
 	ctx.lineTo(350,i*dX);
 	ctx.stroke();
     }
 }
-
-
-for (i=0;i<nbX;i++) {
-    var ligne=[];
-    for (j=0;j<nbY;j++) {
-	ligne.push(false);
-    }
-    tab.push(ligne);
 }
 
-alert(tab);
-
-function dessinGrille(nbX,nbY,mon_canva){}
-
 function ajoutObjet(objet) {
- 
+    obj.push(objet);
+    nbCur+=1;
+    tab[objet[0],[objet[1]]=true;
 }
 
 function dessin(){}
