eff0516ae595e73f48748569d099eb1fd2b7743e
Olivier0123 <45844642+Olivier0123@users.noreply.github.com>
2018-12-13 12:05:43 +0100

    Add files via upload

--- tools.js
+++ tools.js
@@ -1,60 +1,45 @@
 function truncate(val)
 {
-	return (intval = val - val%1);
+    return (intval = val - val%1);
 }
-
 function getRandomInt(min, max)
 {
-   return Math.floor(Math.random() * (max - min + 1)) + min;
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
 function getRandom(min, max)
 { return (Math.random() * (max-min+1)) + min; }
 
-
 function Grille(nbX,nbY,mon_canvas) {
-    
-    var nbC=nbX*nbY;
-    var nbCur=0;
-    var obj=[];
-    
-    var tab=[];
-    for (i=0;i<nbX;i++) {
+    tab=[];
+    for(i=0;i<nbX;i++){
 	var ligne=[];
-	for (j=0;j<nbY;j++) {
+	for(j=0;j<nbY;j++){
 	    ligne.push(false);
 	}
 	tab.push(ligne);
     }
-    return(tab);
+    return(tab);   
 }
 
 
-
-function dessinGrille(nbX,nbY,mon_canvas){
+function dessinGrille(nbX,nbY,mon_canva){
     var dX= 350/nbX;
     var dY=350/nbY;
     
-					  
     for(i=1;i<nbY;i++){
-      ctx.moveTo(i*dY,0);
-      ctx.lineTo(i*dY,350);
-      ctx.stroke();
-  }
-    
-    for(i=1;i<nbX;i++){
-	ctx.moveTo(0,i*dX);
-	ctx.lineTo(350,i*dX);
+	ctx.moveTo(i*dY,0);
+	ctx.lineTo(i*dY,350);
 	ctx.stroke();
     }
-}
-}
+    
+    for(j=1;j<nbX;j++)
+    {
+	ctx.moveTo(0,j*dX);
+	ctx.lineTo(350,j*dX);
+	ctx.stroke();
+    }}
 
-function ajoutObjet(objet) {
-    obj.push(objet);
-    nbCur+=1;
-    tab[objet[0],[objet[1]]=true;
-}
+function ajoutObjet(objet){}
 
 function dessin(){}
 
@@ -63,9 +48,6 @@ function mvtAdmis(objet,dx,dy){}
 function majDessinObjet(objet,dx,dy){}
 
 function stopObjets() {}
-
-
-
 
 function Robot(x,y,vitesse,couleur){}
 
@@ -78,3 +60,4 @@ function reflexion(tabMvt){}
 function action(act){}
 
 function reveille(){}
+
